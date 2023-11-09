0bec79b0ef0da6738dbe4f76c5685e6a73c62bd7
Jitesh4565 <73875204+Jitesh4565@users.noreply.github.com>
2023-11-09 17:14:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
-function calculateMinCost() {
+function calculateMinCost(ropes) {
   //your code here
-  
-  
-  
+  const Arr=ropes.split(',').map(Number);
+   if(ropes.length<2)
+   {
+	   return 0;
+   }
+
+	const totalCost=ropes.reduce((accumulator,currentValue)=>{
+		ropes.sort((a,b)=>a-b);
+   const combinedCost=ropes[0]+ropes[1];
+		ropes.splice(0,2);
+		return accumulator+combinedCost;
+	},0);
+
+	const result=document.getElementById(result);
+	result.textContent=totalCost;
 }  
